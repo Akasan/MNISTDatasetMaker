5bf6ee2c450b3f52a0fc962ef41acd103cf03837
Akasan <daisuke1024akagawa@gmail.com>
2021-11-26 00:09:46 +0900

    Implement function for creating dataloader

--- MNISTDataset.py
+++ MNISTDataset.py
@@ -1,10 +1,21 @@
-from typing import List
-from torch.utils.data import Dataset
+from typing import List, Dict, Union
+from torch.utils.data import Dataset, DataLoader
 from torchvision.datasets import MNIST
 
 
-def get_mnist_dataset(*args, **kwargs):
-    return MNIST(*args, **kwargs)
+DATASET_PARAM = {
+    "root": "./data",
+    "train": True,
+    "download": True,
+    "transform": None
+}
+
+DATALOADER_PARAM = {
+    "batch_size": 128,
+    "shuffle": True,
+    "num_workers": 1,
+    "pin_memory": True
+}
 
 
 class SingleLabelMNIST(Dataset):
@@ -14,12 +25,14 @@ class SingleLabelMNIST(Dataset):
         self._get_dataset(dataset)
 
     def _get_dataset(self, dataset: Dataset):
-        self.dataset = []
-        append = self.dataset.append
+        d = []
+        append = d.append
 
         for image, label in dataset:
             if label == self.LABEL:
                 append(image)
+
+        self.dataset = tuple(d)
 
     def __getitem__(self, idx):
         assert idx < len(self)
@@ -30,18 +43,20 @@ class SingleLabelMNIST(Dataset):
 
 
 class MultiLabelMNIST(Dataset):
-    def __init__(self, dataset: Dataset, label: List[int]):
+    def __init__(self, dataset: Dataset, labels: List[int]):
         super().__init__()
-        self.LABEL = label
+        self.LABEL = labels
         self._get_dataset(dataset)
 
     def _get_dataset(self, dataset: Dataset):
-        self.dataset = []
-        append = self.dataset.append
+        d = []
+        append = d.append
 
         for image, label in dataset:
             if label in self.LABEL:
                 append((image, label))
+
+        self.dataset = tuple(d)
 
     def __getitem__(self, idx):
         assert idx < len(self)
@@ -49,3 +64,15 @@ class MultiLabelMNIST(Dataset):
 
     def __len__(self) -> int:
         return len(self.dataset)
+
+
+def get_mnist_dataset(dataset_param: Dict = DATASET_PARAM):
+    return MNIST(**dataset_param)
+
+
+def get_mnist_dataloader(label: Union[int, List[int]],
+                         dataset_param: Dict = DATASET_PARAM,
+                         dataloader_param: Dict = DATALOADER_PARAM):
+    d = get_mnist_dataset(dataset_param)
+    d = SingleLabelMNIST(d, label) if isinstance(label, int) else MultiLabelMNIST(d, label)
+    return DataLoader(d, **dataloader_param)
